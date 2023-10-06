cee207fbca9bf7b4f63862a2c9610bba0b8f54ee
devZeus MAthew <devzeus.mathew721@gmail.com>
2023-10-06 16:01:06 +0530

    done

---
+++ learnings/ex01.php
@@ -0,0 +1,14 @@
+<?php
+// Part 1: add the name and age variables.
+$name = 'James M';
+$age = 24;
+echo "Hello $name. You are $age years old.\n";
+
+// Part 2: sum up the variables x and y and
+// put the result in the sum variable.
+$x = 195793;
+$y = 256836;
+$sum = $x + $y;
+
+echo "The sum of $x and $y is $sum."
+?>
