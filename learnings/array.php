c1d392455d6a438586578185088bb72782aa007e
devZeus MAthew <devzeus.mathew721@gmail.com>
2023-10-06 21:26:56 +0530

    done

--- learnings/array.php
+++ learnings/array.php
@@ -8,6 +8,16 @@ echo $oddno[4];
 //adding new no in array
 $oddno[5] = 9;
 print_r($oddno);
+//delete a member
+unset($oddno[2]);
+print_r($oddno);
+//count fn
+echo count($oddno);
+//reset fn
+$fn = reset($oddno);
+echo $fn;
+
+
 
 
 
